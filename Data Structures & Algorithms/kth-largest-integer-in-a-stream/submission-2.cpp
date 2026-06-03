136a5099cb70b1fd7d46622ecb4396b618a0729b
Ishaan-Chandak <103961770+Ishaan-Chandak@users.noreply.github.com>
2026-06-04 02:27:01 +0530

    Add: kth-largest-integer-in-a-stream - submission-2

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-2.cpp
@@ -0,0 +1,35 @@
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> q;
+    int k_g;
+    KthLargest(int k, vector<int>& nums) {
+        k_g = k;
+        // for(auto i: nums) q.push(i);
+        for(auto i: nums) {
+            q.push(i);
+            if(q.size() > k) q.pop();
+        }
+    }
+    
+    int add(int val) {
+        q.push(val);
+
+        // vector<int> temp;
+
+        // for(int i=0; i<k_g-1; i++) {
+        //     temp.push_back(q.top());
+        //     q.pop();
+        // }
+
+        // int ans = q.top();
+
+        // for(auto i : temp) q.push(i);
+
+        // return ans;
+
+        if(q.size() > k_g) q.pop();
+
+        return q.top();
+    
+    }
+};

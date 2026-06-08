ff00e816a1b9ee6c1e8672a2f6590b3278bde4b0
Ishaan-Chandak <103961770+Ishaan-Chandak@users.noreply.github.com>
2026-06-08 19:43:25 +0530

    Add: max-area-of-island - submission-6

---
+++ Data Structures & Algorithms/max-area-of-island/submission-6.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    
+    int bfs(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& visited) {
+        queue<pair<int, int>> q;
+        q.push({i, j});
+        vector<int> dx = {-1, 0, 1, 0};
+        vector<int> dy = {0, 1, 0, -1};
+        int n = grid.size();
+        int m = grid[0].size();
+        int area = 0;
+        while(!q.empty()) {
+            auto curr = q.front();
+            q.pop();
+            if(visited[curr.first][curr.second] == 0) {
+                visited[curr.first][curr.second] = 1;
+                area++;
+                for(int i=0; i<4; i++) {
+                    int newx = curr.first + dx[i]; 
+                    int newy = curr.second + dy[i];
+
+                    if(newx >= 0 and newx < n and newy >= 0 and newy < m) {
+                        if(visited[newx][newy] == 0 and grid[newx][newy] == 1) {
+                            q.push({newx, newy});
+                        }
+                    } 
+                }
+            }
+        }
+
+        return area;
+    }
+
+    int dfs(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& visited) {
+        if(i < 0 or j < 0 or i >= grid.size() or j >= grid[0].size() or grid[i][j] == 0 or visited[i][j] == 1) return 0;
+
+        visited[i][j] = 1;
+        vector<int> dx = {-1, 0, 1, 0};
+        vector<int> dy = {0, 1, 0, -1};
+        int res = 1;
+        for(int k=0; k<4; k++) {
+            int newx = i + dx[k]; 
+            int newy = j + dy[k];
+
+            res += dfs(newx, newy, grid, visited);
+        }
+
+        return res;
+    }
+
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        vector<vector<int>> visited(grid.size(), vector<int>(grid[0].size(), 0));
+        int res = 0;
+
+        for(int i=0; i<grid.size(); i++) {
+            for(int j=0; j<grid[0].size(); j++) {
+                if(grid[i][j] == 1 and visited[i][j] == 0) {
+                    // int curr = bfs(i, j, grid, visited);
+                    int curr = dfs(i, j, grid, visited);
+                    res = max(res, curr);
+                }
+            }
+        }
+
+        return res;
+    }
+};

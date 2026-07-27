54420af524e6d88d1fc17be3b08c044c5ad3128e
Ishaan-Chandak <103961770+Ishaan-Chandak@users.noreply.github.com>
2026-07-28 01:53:14 +0530

    Add: combination-sum-iv - submission-0

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int f(vector<int>& nums, int target, vector<int>& dp) {
+        if(target < 0) return 0;
+
+        if(target == 0) { 
+            return 1;
+        }
+
+        if(dp[target] != -1) return dp[target];
+
+        int ans = 0;
+        for(auto num : nums) {
+            ans += f(nums, target - num, dp);
+        }
+
+        return dp[target] = ans;
+    }
+    
+    int combinationSum4(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> dp(target + 1, -1);
+        return f(nums, target, dp);
+    }
+};
